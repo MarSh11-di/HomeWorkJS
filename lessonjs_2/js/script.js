2bc4a455df0095c3d9956c5debf48d352459eb1d
MarSh11-di <112892986+MarSh11-di@users.noreply.github.com>
2023-02-12 11:58:51 +0100

    2

---
+++ lessonjs_2/js/script.js
@@ -0,0 +1,11 @@
+"use strict"
+//document.write("Hello Mariia!")
+/*let nameUser = prompt("Як вас звати?", "Гiсть");
+document.write(`<h1>Hello ${nameUser}</h1> `)*/
+/*let a = parseInt(prompt("a=", "0"));
+let b = parseInt(prompt("b=", "0"));
+let s = a / b;
+document.write(`<h1>S = ${s.toFixed(2)}</h1>`);*/
+/*let max = 78, min = 10;
+let a = min + Math.floor(Math.random() * (max - min + 1))
+console.log(a);*/
